264de847e1105fc5e7c1c2f2879e82ac240d0204
Timo <timokleger@hotmail.com>
2023-07-23 21:02:30 +0200

    google chrome browser fix

--- weather.js
+++ weather.js
@@ -2,34 +2,43 @@ console.log("weather.js loaded");
 
 function changePeriod(form) {
     console.log('Period changed...');
+    // does not work
+    if(!document.getElementById("temperatur_grafana")) 
+        return false;
     // change grafana diagram attribute
-    if(document.getElementById("day_form").checked) {
-        document.getElementById("temperatur_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=6');
-        document.getElementById("wind_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=8');
-        document.getElementById("rain_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=14');
-        document.getElementById("pressure_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=12');
-        document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=10');
+    else if(document.getElementById("day_form").checked) {
+        // this does not work for google chrome browser
+        // document.getElementById("temperatur_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=6');
+        // document.getElementById("wind_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=8');
+        // document.getElementById("rain_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=14');
+        // document.getElementById("pressure_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=12');
+        // document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=10');
+        document.getElementById("temperatur_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=6"');
+        document.getElementById("wind_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=8"'); 
+        document.getElementById("rain_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=14"');
+        document.getElementById("pressure_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=12"');
+        document.getElementById("windDir_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=10"');
     }
     else if(document.getElementById("week_form").checked) {
-        document.getElementById("temperatur_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=6');
-        document.getElementById("wind_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=8');
-        document.getElementById("rain_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=14');
-        document.getElementById("pressure_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=12');
-        document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=10');
+        document.getElementById("temperatur_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=6"');
+        document.getElementById("wind_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30&from=now-7d&to=now&theme=light&panelId=8"');
+        document.getElementById("rain_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&from=now-7d&to=now&theme=light&panelId=14"');
+        document.getElementById("pressure_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-7d&to=now&theme=light&panelId=12"');
+        document.getElementById("windDir_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-7d&to=now&theme=light&panelId=10"');
     }
     else if(document.getElementById("month_form").checked) {
-        document.getElementById("temperatur_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=6');
-        document.getElementById("wind_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=8');
-        document.getElementById("rain_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=14');
-        document.getElementById("pressure_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=12');
-        document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=10');
+        document.getElementById("temperatur_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=6"');
+        document.getElementById("wind_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30&from=now-30d&to=now&theme=light&panelId=8"');
+        document.getElementById("rain_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&from=now-30d&to=now&theme=light&panelId=14"');
+        document.getElementById("pressure_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-30d&to=now&theme=light&panelId=12"');
+        document.getElementById("windDir_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-30d&to=now&theme=light&panelId=10"');
     }
     else if(document.getElementById("year_form").checked) {
-        document.getElementById("temperatur_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=6');
-        document.getElementById("wind_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30&from=now-365d&to=now&theme=light&panelId=8');
-        document.getElementById("rain_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&from=now-365d&to=now&theme=light&panelId=14');
-        document.getElementById("pressure_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-365d&to=now&theme=light&panelId=12');
-        document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-365d&to=now&theme=light&panelId=10');
+        document.getElementById("temperatur_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=6"');
+        document.getElementById("wind_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30&from=now-365d&to=now&theme=light&panelId=8"');
+        document.getElementById("rain_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&from=now-365d&to=now&theme=light&panelId=14"');
+        document.getElementById("pressure_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-365d&to=now&theme=light&panelId=12"');
+        document.getElementById("windDir_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-365d&to=now&theme=light&panelId=10"');
     }
     return false;
   }
