9422721fddf4624ab7d87691c4da5f84de9d165f
Timo <49121144+ChlaegerIO@users.noreply.github.com>
2025-10-12 00:05:56 +0200

    current values

--- weather.js
+++ weather.js
@@ -12,31 +12,47 @@
         // document.getElementById("windDir_grafana").outerHTML.replace(/data="(.+?)"/, 'data="https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=10"');
 
           document.getElementById("temperatur_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=panel-1');
-          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=panel-2');
+          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=panel-7');
           document.getElementById("rain_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=panel-3');
-          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=panel-4');
+          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=panel-2');
           // document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-24h&to=now&theme=light&panelId=10');
+          document.getElementById("current_grafana_temp").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-24h&to=now&timezone=browser&theme=light&panelId=panel-11&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_hum").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-24h&to=now&timezone=browser&theme=light&panelId=panel-13&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_wind").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-24h&to=now&timezone=browser&theme=light&panelId=panel-12&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_rain").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-24h&to=now&timezone=browser&theme=light&panelId=panel-15&__feature.dashboardSceneSolo=true');
       }
       else if(document.getElementById("week_form").checked) {
           document.getElementById("temperatur_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=panel-1');
-          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=panel-2');
+          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=panel-7');
           document.getElementById("rain_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=panel-3');
-          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=panel-4');
+          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=panel-2');
           // document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-7d&to=now&theme=light&panelId=10');
+          document.getElementById("current_grafana_temp").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-7d&to=now&timezone=browser&theme=light&panelId=panel-11&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_hum").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-7d&to=now&timezone=browser&theme=light&panelId=panel-13&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_wind").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-7d&to=now&timezone=browser&theme=light&panelId=panel-12&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_rain").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-7d&to=now&timezone=browser&theme=light&panelId=panel-15&__feature.dashboardSceneSolo=true');
       }
       else if(document.getElementById("month_form").checked) {
           document.getElementById("temperatur_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=panel-1');
-          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=panel-2');
+          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=panel-7');
           document.getElementById("rain_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=panel-3');
-          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=panel-4');
+          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=panel-2');
           // document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?orgId=1&refresh=30s&from=now-30d&to=now&theme=light&panelId=10');
+          document.getElementById("current_grafana_temp").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-30d&to=now&timezone=browser&theme=light&panelId=panel-11&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_hum").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-30d&to=now&timezone=browser&theme=light&panelId=panel-13&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_wind").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-30d&to=now&timezone=browser&theme=light&panelId=panel-12&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_rain").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-30d&to=now&timezone=browser&theme=light&panelId=panel-15&__feature.dashboardSceneSolo=true');
       }
       else if(document.getElementById("year_form").checked) {
           document.getElementById("temperatur_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=panel-1');
-          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=panel-2');
+          document.getElementById("wind_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=panel-7');
           document.getElementById("rain_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=panel-3');
-          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=panel-4');
+          document.getElementById("pressure_grafana").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&refresh=30s&from=now-365d&to=now&theme=light&panelId=panel-2');
           // document.getElementById("windDir_grafana").setAttribute('data', 'https://kleger.vsos.ethz.ch/d-solo/NKXNbOk4z/main-weather-parameters?from=now-365d&to=now&theme=light&panelId=10');
+          document.getElementById("current_grafana_temp").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-365d&to=now&timezone=browser&theme=light&panelId=panel-11&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_hum").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-365d&to=now&timezone=browser&theme=light&panelId=panel-13&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_wind").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-365d&to=now&timezone=browser&theme=light&panelId=panel-12&__feature.dashboardSceneSolo=true');
+          document.getElementById("current_grafana_rain").setAttribute('data', 'https://wetter.timokleger.com/d-solo/adk6z49/wetter?orgId=1&from=now-365d&to=now&timezone=browser&theme=light&panelId=panel-15&__feature.dashboardSceneSolo=true');
       }
       return false;
     }
